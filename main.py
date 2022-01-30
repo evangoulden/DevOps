d564da5fbf8272909731b0fc8e7c4edf3b3136bb
evan.goulden <evan.j.goulden@gmail.com>
2022-01-30 20:01:33 +0000

    Added Holiday Timetable

--- main.py
+++ main.py
@@ -2,11 +2,13 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import Person
 
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    Person.names()
 
 
 # Press the green button in the gutter to run the script.
